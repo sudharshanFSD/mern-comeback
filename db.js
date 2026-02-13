a47d236f6d84bed3d29e8d4aa5464befe6aab83e
sudharshanFSD <tsrssudharshan>
2026-02-13 15:07:10 +0530

    auth system

--- db.js
+++ db.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
 
-const connectDB = async()=>{
+const connectDB= async()=>{
     try{
         await mongoose.connect("mongodb://localhost:27017");
-        console.log('MongoDB connected successfuly');
+        console.log("MongoDB connected successfully");
     }catch(error){
         console.error(error);
         process.exit(1);
     }
 };
 
-module.exports = connectDB
+module.exports = connectDB;
 
 
 
